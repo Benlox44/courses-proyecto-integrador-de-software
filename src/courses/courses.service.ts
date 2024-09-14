7833ed3bc8ec502688405ad0c5dff7c23f5bd998
Gilberto Muñoz Benjamín Ignacio <benjagilberto44@gmail.com>
2024-09-14 15:00:58 -0300

    correo con nombre de cursos

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -13,6 +13,7 @@ export class CoursesService implements OnModuleInit {
 
   async onModuleInit() {
     this.listenForCourseRequests();
+    this.listenForPurchaseRequests(); // Escuchar las solicitudes de PurchaseService
   }
 
   private listenForCourseRequests() {
@@ -40,13 +41,56 @@ export class CoursesService implements OnModuleInit {
             const responseQueue = msg.properties.replyTo;
             const correlationId = msg.properties.correlationId;
 
-
             channel.sendToQueue(responseQueue, Buffer.from(JSON.stringify(courses)), {
               correlationId,
             });
-
           } catch (error) {
             console.error('Error al obtener los detalles de los cursos:', error);
+          }
+
+          channel.ack(msg);
+        });
+      });
+    });
+  }
+
+  // Nuevo método para escuchar la cola de PurchaseService
+  private listenForPurchaseRequests() {
+    amqp.connect('amqp://localhost', (error0, connection) => {
+      if (error0) {
+        throw error0;
+      }
+
+      connection.createChannel((error1, channel) => {
+        if (error1) {
+          throw error1;
+        }
+
+        const queue = 'purchase_course_queue';
+
+        channel.assertQueue(queue, { durable: false });
+
+        channel.consume(queue, async (msg) => {
+          if (!msg) return;
+
+          const { courseIds } = JSON.parse(msg.content.toString());
+
+          try {
+            // Obtener los títulos y precios de los cursos para enviar de vuelta a PurchaseService
+            const courses = await this.findByIds(courseIds);
+            const courseDetails = courses.map(course => ({
+              title: course.title,
+              price: course.price,
+            }));
+
+            const responseQueue = msg.properties.replyTo;
+            const correlationId = msg.properties.correlationId;
+
+            channel.sendToQueue(responseQueue, Buffer.from(JSON.stringify(courseDetails)), {
+              correlationId,
+            });
+          } catch (error) {
+            console.error('Error al obtener los detalles de los cursos para la compra:', error);
           }
 
           channel.ack(msg);
